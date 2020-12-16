e50701ac95908446b44aae63a81d75ef3d92df42
skyneton <67633420+skyneton@users.noreply.github.com>
2020-12-16 19:36:13 +0900

    Minecraft Screen Share

---
+++ src/GetDesktop/CommandManager.java
@@ -0,0 +1,226 @@
+package GetDesktop;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.ItemFrame;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.MapMeta;
+
+public class CommandManager implements CommandExecutor {
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		// TODO Auto-generated method stub
+		if(!(sender instanceof Player)) {
+			sender.sendMessage("GetDesktop:: 콘솔에서는 사용할 수 없습니다.");
+			return true;
+		}
+		if(args.length > 0) {
+			if(args[0].equalsIgnoreCase("web")) {
+				if(args.length > 1) {
+					try {
+						if(args[1].contains("http://") | args[1].contains("https://"))
+							Desktop.getDesktop().browse(new URI(args[1]));
+						else
+							Desktop.getDesktop().browse(new URI("http://"+args[1]));
+						sender.sendMessage("GetDesktop:: 웹 페이지를 성공적으로 띄웠습니다.");
+					} catch (IOException | URISyntaxException e) {
+						// TODO Auto-generated catch block
+						sender.sendMessage("웹 페이지를 띄우는데 실패하였습니다.");
+						Bukkit.getLogger().info("GetDesktop:: 웹 페이지를 띄우는데 실패하였습니다.");
+					}
+					return true;
+				}
+			}
+			
+			if(args[0].equalsIgnoreCase("start")) {
+				if(!GetDesktop.threadManager.runable) {
+					GetDesktop.threadManager.Start();
+					sender.sendMessage("GetDesktop:: 시작되었습니다.");
+				}else
+					sender.sendMessage("GetDesktop:: 이미 시작되었습니다.");
+				return true;
+			}
+			
+			if(args[0].equalsIgnoreCase("stop")) {
+				if(GetDesktop.threadManager.runable) {
+					GetDesktop.threadManager.Exit();
+					sender.sendMessage("GetDesktop:: 종료되었습니다.");
+					return true;
+				}
+				sender.sendMessage("GetDesktop:: 이미 종료되었습니다.");
+				return true;
+			}
+
+			if(args[0].equalsIgnoreCase("get")) {
+				ItemStack item = new ItemStack(Material.STICK);
+				ItemMeta meta = item.getItemMeta();
+				meta.setDisplayName("§aPos§f 지정 막대기");
+				meta.setLore(Arrays.asList("§f좌/우 클릭으로 §aPOS§f를 지정할 수 있습니다.", "§f지정후 build를 통해 액자를 그릴수 있습니다."));
+				item.setItemMeta(meta);
+				((Player) sender).getInventory().addItem(item);
+
+				item = new ItemStack(Material.BLAZE_ROD);
+				meta = item.getItemMeta();
+				meta.setDisplayName("§c마우스 클릭§f 막대기");
+				meta.setLore(Arrays.asList("§f좌/우 클릭으로 §c마우스를 클릭§f할 수 있습니다."));
+				item.setItemMeta(meta);
+				((Player) sender).getInventory().addItem(item);
+			}
+			
+			if(args[0].equalsIgnoreCase("build")) {
+				if(!EventManager.pos1.containsKey(((Player) sender).getUniqueId()) || !EventManager.pos2.containsKey(((Player) sender).getUniqueId())) {
+					sender.sendMessage("GetDesktop:: 먼저 나무막대기로 위치를 지정해주세요.");
+					return true;
+				}
+
+				Location pos1 = EventManager.pos1.get(((Player) sender).getUniqueId());
+				Location pos2 = EventManager.pos2.get(((Player) sender).getUniqueId());
+				
+				int maxX = (pos1.getBlockX() >= pos2.getBlockX()) ? pos1.getBlockX() : pos2.getBlockX();
+				int minX = pos1.getBlockX() + pos2.getBlockX() - maxX;
+				
+				int maxY = (pos1.getBlockY() >= pos2.getBlockY()) ? pos1.getBlockY() : pos2.getBlockY();
+				int minY = pos1.getBlockY() + pos2.getBlockY() - maxY;
+				
+				int maxZ = (pos1.getBlockZ() >= pos2.getBlockZ()) ? pos1.getBlockZ() : pos2.getBlockZ();
+				int minZ = pos1.getBlockZ() + pos2.getBlockZ() - maxZ;
+
+				int height = GetDesktop.height;
+				height = (height%128 != 0) ? height/128+1 : height/128;
+				
+				int width = GetDesktop.width;
+				width = (width%128 != 0) ? width/128+1 : width/128;
+					
+				if(maxX == minX) {
+					
+					
+					sender.sendMessage("GetDesktop:: 가져오는중..");
+					for(int Y = maxY; Y >= minY; Y--) {
+						if((Y-maxY)*-1+1 > height)
+							break;
+						if(((Player) sender).getLocation().getX() >= maxX) {
+							for(int Z = maxZ; Z >= minZ; Z--) {
+								if((Z-maxZ)*-1+1 > width)
+									break;
+								Location loc = new Location(((Player) sender).getWorld(), maxX, Y, Z);
+								
+								loc.getBlock().setType(Material.DARK_OAK_PLANKS);
+								loc.add(1, 0, 0);
+
+								ItemStack item = new ItemStack(Material.LEGACY_MAP);
+								
+								MapMeta meta = (MapMeta) item.getItemMeta();
+								meta.setMapId(((Y-maxY)*-1) * width + (Z-maxZ)*-1);
+								System.out.println("1: "+((Y-maxY)*-1) +", "+ width + ", "+ (Z-maxZ)*-1);
+								
+								item.setItemMeta(meta);
+								
+								ItemFrameSpawn(loc, item);
+							}
+						}else {
+							for(int Z = minZ; Z <= maxZ; Z++) {
+								if(Z-minZ+1 > height)
+									break;
+								Location loc = new Location(((Player) sender).getWorld(), maxX, Y, Z);
+								
+								loc.getBlock().setType(Material.DARK_OAK_PLANKS);
+								loc.subtract(1, 0, 0);
+
+								ItemStack item = new ItemStack(Material.LEGACY_MAP);
+								
+								MapMeta meta = (MapMeta) item.getItemMeta();
+								meta.setMapId(((Y-maxY)*-1) * width + (Z-minZ));
+								System.out.println("2: "+((Y-maxY)*-1) +", "+ width + ", "+ (Z-minZ));
+								
+								item.setItemMeta(meta);
+								
+								ItemFrameSpawn(loc, item);
+							}
+						}
+					}
+				}
+
+				else if(maxZ == minZ) {
+					sender.sendMessage("GetDesktop:: 가져오는중..");
+					for(int Y = maxY; Y >= minY; Y--) {
+						if((Y-maxY)*-1+1 > height)
+							break;
+						if(((Player) sender).getLocation().getZ() >= maxZ) {
+							for(int X = minX; X <= maxX; X++) {
+								if(X-minX+1 > width)
+									break;
+								Location loc = new Location(((Player) sender).getWorld(), X, Y, maxZ);
+
+								loc.getBlock().setType(Material.DARK_OAK_PLANKS);
+								loc.add(0, 0, 1);
+								
+								
+								ItemStack item = new ItemStack(Material.LEGACY_MAP);
+								
+								MapMeta meta = (MapMeta) item.getItemMeta();
+								meta.setMapId(((Y-maxY)*-1) * width + (X-minX));
+								System.out.println("3: " + ((Y-maxY)*-1) +", "+ width +", "+ (X-minX));
+								
+								item.setItemMeta(meta);
+								
+								ItemFrameSpawn(loc, item);
+							}
+						}else {
+							for(int X = maxX; X >= minX; X--) {
+								if((X-maxX)*-1+1 > height)
+									break;
+								Location loc = new Location(((Player) sender).getWorld(), X, Y, maxZ);
+
+								loc.getBlock().setType(Material.DARK_OAK_PLANKS);
+								loc.subtract(0, 0, 1);
+
+								ItemStack item = new ItemStack(Material.LEGACY_MAP);
+								
+								MapMeta meta = (MapMeta) item.getItemMeta();
+								meta.setMapId(((Y-maxY)*-1) * width + (X-maxX) * -1);
+								System.out.println("4: "+((Y-maxY)*-1) + ", "+ width +", "+ (X-maxX) * -1);
+								
+								item.setItemMeta(meta);
+								
+								ItemFrameSpawn(loc, item);
+							}
+						}
+					}
+				}
+				
+				else
+					sender.sendMessage("GetDesktop:: 위치는 무조건 직선이여야 합니다. (X축 또는 Z축이 같아야 함)");
+				
+				return true;
+			}
+		}
+		sender.sendMessage("/"+label+" build        | 컴퓨터화면을 액자에 그립니다. 막대기로 POS1과 POS2를 정해주세요.");
+		sender.sendMessage("/"+label+" web [site] | 웹을 컴퓨터에 띄웁니다.");
+		sender.sendMessage("/"+label+" stop        | 컴퓨터화면을 정지합니다.");
+		sender.sendMessage("/"+label+" start       | 컴퓨터화면을 시작합니다.");
+		sender.sendMessage("/"+label+" get         | 관련된 아이템을 얻습니다.");
+		return true;
+	}
+	
+	private static void ItemFrameSpawn(Location loc, ItemStack item) {
+		ItemFrame itemFrame = (ItemFrame) loc.getWorld().spawnEntity(loc, EntityType.ITEM_FRAME);
+		
+		itemFrame.setItem(item);
+	}
+
+}
---
+++ src/GetDesktop/ThreadManager.java
@@ -0,0 +1,263 @@
+package GetDesktop;
+
+import java.awt.AWTException;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.image.BufferedImage;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.libs.org.apache.commons.codec.binary.Base64;
+import org.bukkit.craftbukkit.v1_16_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+import net.minecraft.server.v1_16_R1.PacketPlayOutMap;
+
+public class ThreadManager {
+	
+	private static Renderer renderer;
+	
+//	private long time;
+	
+	private Robot r;
+	
+	private Rectangle rec;
+
+	ExecutorService es = Executors.newFixedThreadPool(1);
+	ExecutorService esGetPacket = Executors.newFixedThreadPool(1);
+	ExecutorService esSendPacket = Executors.newFixedThreadPool(1);
+	ExecutorService esCapture = Executors.newFixedThreadPool(1);
+	
+	public boolean runable = false;
+	
+	public HashMap<Integer, int[]> vs = new HashMap<>();
+
+	HashMap<Integer, String> ImgHashLast = new HashMap<>();
+	public HashMap<Integer, PacketPlayOutMap> packets = new HashMap<>();
+	
+	public int width, height;
+	
+	byte[] buf;
+	
+//	public Renderer renderer;
+	
+	public ThreadManager() {
+		try {
+			this.r = new Robot();
+//			rec = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+			rec = new Rectangle(GetDesktop.width, GetDesktop.height);
+			
+			renderer = new Renderer(GetDesktop.width, GetDesktop.height);
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void Start() {
+		runable = true;
+		
+//		time = System.currentTimeMillis();
+
+		height = GetDesktop.height;
+		height = (height%128 != 0) ? height/128+1 : height/128;
+
+		width = GetDesktop.width;
+		width = (width%128 != 0) ? width/128+1 : width/128;
+		
+		esCapture.submit(new CaptureWorker());
+
+		for(int i = 0; i < height; i += 2) {
+			es.submit(new MainWorker(i, width));
+		}
+	}
+	
+	public void Exit() {
+		runable = false;
+	}
+	
+	public void Stop() {
+		runable = false;
+		esSendPacket.shutdown();
+		esGetPacket.shutdown();
+		esCapture.shutdown();
+		es.shutdown();
+		while(!es.isShutdown() | !esGetPacket.isShutdown() | !esCapture.isShutdown() | !esSendPacket.isShutdown());
+	}
+	
+	public String getMD5(byte[] bytes) {
+		
+		try {
+			
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			
+			md.reset();
+			md.update(bytes);
+			
+			Base64 base64 = new Base64();
+			byte[] byteData = base64.encode(md.digest());
+			
+			return new String(byteData);
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	public BufferedImage ImageResize(BufferedImage img, int width, int height) {
+		try {
+			BufferedImage output = new BufferedImage(width, height, img.getType());
+			
+			Graphics2D graphics2d = output.createGraphics();
+			graphics2d.drawImage(img, 0, 0, width, height, null);
+			graphics2d.dispose();
+			
+			return output;
+		}catch(Exception e) {
+			return img;
+		}
+	}
+	
+	class CaptureWorker implements Runnable {
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			while(runable) {
+//				BufferedImage bufferedImage = ImageResize(r.createScreenCapture(rec), GetDesktop.width, GetDesktop.height);
+				BufferedImage bufferedImage = r.createScreenCapture(rec);
+				buf = renderer.render(bufferedImage);
+			}
+		}
+		
+	}
+	
+	class MainWorker implements Runnable {
+		public final int h, width;
+		
+		public MainWorker(int h, int width) {
+			// TODO Auto-generated constructor stub
+			this.h = h;
+			this.width = width;
+		}
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			
+//			while(runable) {
+//				BufferedImage bufferedImage = ImageResize(r.createScreenCapture(rec), GetDesktop.width, GetDesktop.height);
+////				es.submit(new GetPacket(renderer.render(bufferedImage)));
+//				for(int i = 0; i < width; i++) {
+//					Future<?> x = esGetPacket.submit(new GetPacket(bufferedImage, width * h + i));
+//					while(!x.isDone());
+//				}
+//				
+//				long now = System.currentTimeMillis();
+////				System.out.println(now-time+"ms");
+//				time = now;
+//			}
+			
+			Timer timer = new Timer();
+			TimerTask task = new TimerTask() {
+
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+					while(runable) {
+						
+	//					es.submit(new GetPacket(renderer.render(bufferedImage)));
+						for(int i = 0; i < width * 2 && width * h + i < width * height; i++) {
+							Future<?> x = esGetPacket.submit(new GetPacket(width * h + i));
+							while(!x.isDone());
+						}
+	
+//						long now = System.currentTimeMillis();
+//						System.out.println(now-time+"ms");
+//						time = now;
+					}
+				}
+				
+			};
+			
+			timer.schedule(task, 1);
+		}
+		
+	}
+	
+	class GetPacket implements Runnable {
+		
+		int i;
+		
+		public GetPacket(int i) {
+			this.i = i;
+		}
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			byte[] SplitImg = GetSplitImg(vs.get(i)[1] * 128, vs.get(i)[0] * 128);
+			
+//			byte[] SplitImg = MapPalette.imageToBytes(buf.getSubimage(vs.get(i)[1] * 128, vs.get(i)[0] * 128, (GetDesktop.width < vs.get(i)[1] * 128 + 128) ? GetDesktop.width - vs.get(i)[1] * 128 : 128, (GetDesktop.height < vs.get(i)[0] * 128 + 128) ? GetDesktop.height - vs.get(i)[0] * 128 : 128));
+			String hash = getMD5(SplitImg);
+			
+			if(ImgHashLast.containsKey(i)) {
+				if(!ImgHashLast.get(i).equalsIgnoreCase(hash)) {
+					ImgHashLast.replace(i, hash);
+					
+					PacketPlayOutMap packet = new PacketPlayOutMap(i, (byte) 0, false, false, new ArrayList<>(), SplitImg, 0, 0, 128, 128);
+					esSendPacket.submit(new SendPacket(packet));
+					
+					packets.put(i, packet);
+				}
+			}else {
+				ImgHashLast.put(i, hash);
+				
+				PacketPlayOutMap packet = new PacketPlayOutMap(i, (byte) 0, false, false, new ArrayList<>(), SplitImg, 0, 0, 128, 128);
+				esSendPacket.submit(new SendPacket(packet));
+				
+				packets.put(i, packet);
+			}
+		}
+		
+		public byte[] GetSplitImg(int sx, int sy) {
+			byte[] b = new byte[16384];
+			
+			for(int y = 0; y < 128 && sy + y < GetDesktop.height; y++) {
+				for(int x = 0; x < 128 && sx + x < GetDesktop.width; x++) {
+					b[y * 128 + x] = buf[GetDesktop.width * (sy + y) + (sx + x)];
+				}
+			}
+			
+			return b;
+		}
+		
+	}
+	
+	class SendPacket implements Runnable {
+		
+		PacketPlayOutMap packet;
+		
+		public SendPacket(PacketPlayOutMap packet) {
+			this.packet = packet;
+		}
+		
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			for(Player player : Bukkit.getOnlinePlayers())
+				((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
+		}
+	}
+}
